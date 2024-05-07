eb27f7069417b57e508c62809b4f9d2a9d508d2b
manh-tien30 <tien310001@gmail.com>
2024-05-07 16:06:40 +0700

    update lang vn

--- App_Plugins/ManagementSection/TopicDashboard/topic.controller.js
+++ App_Plugins/ManagementSection/TopicDashboard/topic.controller.js
@@ -1,3 +1,26 @@
-angular.module("umbraco").controller("TopicDashboardController", function ($scope) {
-    
-});
+(function () {
+    "use strict";
+
+    function Controller(myService) {
+
+        var vm = this;
+        vm.buttonState = "init";
+
+        vm.clickButton = clickButton;
+
+        function clickButton() {
+
+            vm.buttonState = "busy";
+
+            myService.clickButton().then(function () {
+                vm.buttonState = "success";
+            }, function () {
+                vm.buttonState = "error";
+            });
+
+        }
+    }
+
+    angular.module("umbraco").controller("TopicDashboardController", Controller);
+
+})();
